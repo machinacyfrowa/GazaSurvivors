18800de38bce7eede3ece231dfda8c5e1ce0c083
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-04-02 09:45:25 +0200

    Dodano zarządzanie wrogami i kamerą w grze
    W pliku `PlayerController.cs` dodano przestrzeń nazw `System.Linq` oraz zainicjowano listę `enemies`, która jest wypełniana obiektami z tagiem "Enemy" i sortowana według odległości od gracza.
    
    W pliku `SampleScene.unity` dodano nowy komponent oraz zmieniono lokalną rotację obiektu.
    
    W pliku `CameraController.cs` wprowadzono nową klasę do zarządzania pozycjonowaniem kamery względem gracza, co poprawia wrażenia z gry.
    
    Zaktualizowano plik `CameraController.cs.meta` o informacje o wersji i GUID.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,10 +10,12 @@ public class PlayerController : MonoBehaviour
     //odniesienie do komponentu Rigidbody gracza
     Rigidbody rb;
     // Start is called before the first frame update
+    public List<GameObject> enemies;
     void Start()
     {
         //pobieramy odniesienie do komponentu Rigidbody
         rb = GetComponent<Rigidbody>();
+        enemies = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -24,6 +27,13 @@ public class PlayerController : MonoBehaviour
         //Vector3 movementVector = new Vector3(controllerInput.x, 0, controllerInput.y);
         //przesuwamy obiekt gracza o wektor ruchu
         //transform.Translate(movementVector * Time.deltaTime * speed);
+
+        //wyci¹gamy sobie ze sceny wszystkie obiekty z tagiem Enemy i pakujemy na listê
+        enemies = GameObject.FindGameObjectsWithTag("Enemy").ToList();
+        //sortujemy listê wrogów wed³ug odleg³oœci od gracza i zpaisujemy ponownie jako liste
+        //sk³adnia LINQ - to co jest w nawiasie po orderby czytamy jako
+        //dla ka¿dego wroga w liœcie enemies oblicz odleg³oœæ od gracza i posortuj listê wed³ug tej odleg³oœci
+        enemies = enemies.OrderBy(enemy => Vector3.Distance(enemy.transform.position, transform.position)).ToList();
     }
     void FixedUpdate()
     {
