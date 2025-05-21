81a1c139897508ae643391450d630c54a7d3baca
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-05-21 09:19:28 +0200

    Dodano wzorzec singleton w GameManager i aktualizacje fontu
    W pliku `GameManager.cs` zaimplementowano wzorzec singleton dla klasy `GameManager`, co zapewnia, że w grze może istnieć tylko jedna instancja tej klasy. Dodano metody `Awake` i `Start`, które zarządzają cyklem życia obiektu `GameManager` oraz zapobiegają jego zniszczeniu przy zmianie sceny. Usunięto oryginalną, pustą metodę `Start`.
    
    W pliku `CharacterSelection.unity` usunięto komponenty związane z obiektem `GameManager`, co sugeruje jego usunięcie z hierarchii sceny. W sekcji `SceneRoots` również usunięto odniesienie do obiektu `GameManager`.
    
    W pliku `LiberationSans SDF - Fallback.asset` zaktualizowano dane fontu i tekstur. Dodano szczegółowe informacje o glifach oraz zaktualizowano wymiary tekstury z 1x1 do 512x512. Wprowadzono nową właściwość `m_StreamData`, co może sugerować przyszłe wykorzystanie strumieni danych do ładowania tekstur.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,11 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    //to jest instancja (wyst¹pienie) klasy GameManager
+    //jedyne s³uszne - zawsze w programie mo¿e byæ tylko jeden
+    //GameManager - singleton
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        //je¿eli w grze jest ju¿ GameManager i nie jest to ten sam obiekt
+        //który w³aœnie uruchomiliœmy to go usuwamy nadmiarowy (nowy)
+        //to siê wywo³a jeœli przypadkiem zrobimy drugi GameManager
+        if (Instance != null && Instance != this)
+        {
+            //niszczymy obecny (nieprawid³owy) obiekt GameManager
+            Destroy(gameObject);
+            //wychodzimy z funkcji
+            return;
+        }
+        //je¿eli nie ma GameManagera to zapisujemy ten obiekt jako instancjê
+        if(Instance == null)
+            Instance = this;
+
+        //ta funkcja zpobiega usuniêciu gameManagera z gry przy zmianie sceny
+        DontDestroyOnLoad(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        //ta funkcja zpobiega usuniêciu gameManagera z gry przy zmianie sceny
-        DontDestroyOnLoad(gameObject);
+        
     }
     public void NewGame()
     {
